9321de2981bf2eb7236022fe0ae5274b1a52384e
pravin-sivabalan <pravinsivabalan@gmail.com>
2018-04-09 16:30:34 -0400

    Switch to jwt

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,19 +4,23 @@ let morgan = require('morgan');
 let bodyParser = require('body-parser');
 let session = require('express-session');
 let MongoDBStore = require('connect-mongodb-session')(session);
+let jwt = require('express-jwt');
 let store = new MongoDBStore({ uri: process.env.DB, collection: 'mySessions' });
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.use(require('express-session')({
-  secret: 'SessionSecret',
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 // 1 day
-  },
-  store: store,
-  resave: true,
-  saveUninitialized: true
+app.use(jwt({
+  secret: process.env.SECRET,
+  credentialsRequired: false,
+  getToken: (req) => {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+      return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
+    }
+    return null;
+  }
 }));
 
 app.use('/api/auth', require('./auth-router'));
