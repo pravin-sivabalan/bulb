d0b217e7b409abbbb86b3a336ce2a216b5d92da9
pravin-sivabalan <pravinsivabalan@gmail.com>
2018-04-11 12:30:49 -0400

    move render to frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,13 @@ let app = require('./routes/index');
 app.server = http.createServer(app);
 mongoose.connect(process.env.DB, { useMongoClient: true });
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../frontend/build/index.html'));
+  });
+}
+
 app.server.listen(process.env.PORT, () => {
 		console.log(`Started on port ${app.server.address().port}`);
 });
