0428f6ca806f039510ddb44d2f7348dea51be9aa
pravin-sivabalan <pravinsivabalan@gmail.com>
2018-04-16 14:42:19 -0400

    Add get idea and delete idea

--- backend/src/routes/idea-router.js
+++ backend/src/routes/idea-router.js
@@ -1,8 +1,30 @@
 const { successRes, errorRes } = require('../utils');
 const express = require('express')
 const Idea = require('../models/idea-model');
-const Authorized = require('../utilities/middleware');
+const Authorized = require('../utils/middleware');
 const router = express.Router();
+
+router.get('/:id', Authorized, (req, res) => {
+  Idea.findById(req.params.id, (err, idea) => {
+      if(err) return errorRes(res, 500, 'MongoError');
+      if(!idea) return errorRes(res, 404, 'Idea not found');
+      return successRes(res, {
+        idea: idea
+      });
+  });
+});
+
+router.delete('/:id', Authorized, (req, res) => {
+  Idea.findById(req.params.id, (err, idea) => {
+      if(err) return errorRes(res, 500, 'MongoError');
+      if(!idea) return errorRes(res, 404, 'Idea not found');
+      if(req.user.id != idea._user) return errorRes(res, 401, 'User is unauthorized to delete this idea');
+      Idea.findByIdAndRemove(req.params.id, (err, idea) => {
+        if(err) return errorRes(res, 500, 'MongoError');
+        return successRes(res, 'Successfully deleted idea');
+      });
+  });
+});
 
 router.post('/', Authorized, (req, res) => {
   if(!req.body.title) return errorRes(res, 400, 'Idea must have a title');
@@ -20,7 +42,6 @@ router.post('/', Authorized, (req, res) => {
       idea: newIdea
     });
   });
-
 });
 
 module.exports = router;
--- backend/src/routes/user-router.js
+++ backend/src/routes/user-router.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let router = express.Router()
 let User = require('../models/user-model');
-let Authorized = require('../utilities/middleware');
+let Authorized = require('../utils/middleware');
 
 router.get('/', Authorized, (req, res) => {
     User.findById(req.user.id, (err, user) => {
