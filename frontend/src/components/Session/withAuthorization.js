2350908c44dea0fc63fef2c629d4da8b09bfd3f5
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2018-04-22 16:43:26 -0400

    Added auth state changed listener

--- frontend/src/components/Session/withAuthorization.js
+++ frontend/src/components/Session/withAuthorization.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
+import { localStorageChanged } from '../../actions';
 import * as routes from '../../constants';
 
 const withAuthorization = (props) => (Component) => {
   class WithAuthorization extends React.Component {
     render() {
-      const token = JSON.parse(this.props.token);
-      console.log('withAuthorization token:', token);
-      return token ? <Component {...this.props} {...props}/> : null;
+      return this.props.token ? <Component {...this.props} {...props}/> : null;
     }
   }
 
   const mapStateToProps = (store) => ({
     token: store.sessionState.token,
   });
+
   return compose(
     withRouter,
-    connect(mapStateToProps, { }),
+    connect(mapStateToProps, { localStorageChanged }),
   )(WithAuthorization);
 }
 
