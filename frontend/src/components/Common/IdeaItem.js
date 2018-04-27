f0e5dcfe04a195147a0f906b329feb93aa3da2aa
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2018-04-27 13:03:34 -0400

    Plz fix the signup issue

--- frontend/src/components/Common/IdeaItem.js
+++ frontend/src/components/Common/IdeaItem.js
@@ -9,7 +9,7 @@ import { deleteIdea, likeIdea, unLikeIdea } from '../../actions';
 class IdeaItem extends Component {
 	constructor(props) {
 		super(props);
-		const liked = this.props.user && this.props.user.likes.find(like => like._id === this.props.idea._id);
+		const liked = this.props.user && this.props.user.likes && this.props.user.likes.find(like => like._id === this.props.idea._id);
 		const { likes } = this.props.idea;
 		const sameUser = this.props.user && this.props.user._id == this.props.idea._user._id;
 		this.state = {
