60907b4f70c7176aa78b2665e61c3d8938779b6b
Mihir Tiwari <mtiw999@gmail.com>
2018-04-26 23:09:39 -0400

    made more mobile friendly

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,27 +3,81 @@ import { compose } from 'recompose';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { SignOutButton } from '../Common';
-import { Menu, Header, Button, Search } from 'semantic-ui-react';
+import { Menu, Header, Button, Search, Responsive, Sidebar, Form } from 'semantic-ui-react';
 import { signOut } from '../../actions';
 import * as routes from '../../constants';
 
-const NavigationAuth = ({ signOut }) =>
-  <Menu fixed='top' fluid compact stackable>
-    <Menu.Item as={ Link } name='Bulb' to={routes.HOME}><Header as='h3' color='blue'>Bulb</Header></Menu.Item>
-    <Menu.Item><Search/></Menu.Item>
-    <Menu.Item as={ Link } name='Home' to={routes.HOME}>Home</Menu.Item>
-    <Menu.Item as={ Link } name='CreateIdea' to={routes.CREATE_IDEA}>Create Idea</Menu.Item>
-    {/*<Menu.Item as={ Link }  name='Account' to={routes.ACCOUNT}>Account</Menu.Item>*/}
-    <Menu.Item position='right' as={ Link } name='Settings' to={routes.SETTINGS}>Settings</Menu.Item>
-    <Menu.Item as={ Link } name="SignOut" to={routes.LOGIN} onClick={signOut} >Sign Out</Menu.Item>
-  </Menu>
+let visible = false;
 
+const toggle = () => {
+  visible = !visible;
+  console.log(visible)
+}
+
+const NavigationAuth = ({ signOut }) =>
+  <div>
+    <Responsive as={Menu} {...Responsive.onlyComputer}>
+      <Menu fluid compact>
+        <Menu.Item as={ Link } name='Bulb' to={routes.HOME}><Header as='h3' color='blue'>Bulb</Header></Menu.Item>
+        <Menu.Item><Search/></Menu.Item>
+        <Menu.Item as={ Link } name='Home' to={routes.HOME}>Home</Menu.Item>
+        <Menu.Item as={ Link } name='CreateIdea' to={routes.CREATE_IDEA}>Create Idea</Menu.Item>
+        {/*<Menu.Item as={ Link }  name='Account' to={routes.ACCOUNT}>Account</Menu.Item>*/}
+        <Menu.Item  as={ Link } name='Settings' to={routes.SETTINGS}>Settings</Menu.Item>
+        <Menu.Item position='right' as={ Link } name="SignOut" to={routes.LOGIN} onClick={signOut} >Sign Out</Menu.Item>
+      </Menu>
+    </Responsive>
+    <Responsive as={Menu} {...Responsive.onlyMobile}>
+      <Menu fluid vertical>
+        <Menu.Menu style={{ marginBottom:'10px' }}>
+          <Menu.Item as={ Link } name='Bulb' to={routes.HOME}>
+            <Header style={{ textAlign:'center' }} as='h2' color='blue'>Bulb</Header>
+          </Menu.Item>
+          <Menu.Item>
+            <Search style={{ textAlign:'center' }} fluid/>
+          </Menu.Item>
+        </Menu.Menu>
+        <Menu.Item className='center' as={ Link } name='Home' to={routes.HOME}>
+          <p style={{textAlign:'center'}}>Home</p>
+        </Menu.Item>
+        <Menu.Item className='center' as={ Link } name='CreateIdea' to={routes.CREATE_IDEA}>
+          <p style={{textAlign:'center'}}>Create Idea</p>
+        </Menu.Item>
+        {/*<Menu.Item as={ Link }  name='Account' to={routes.ACCOUNT}>Account</Menu.Item>*/}
+        <Menu.Item className='center' as={ Link } name='Settings' to={routes.SETTINGS}>
+          <p style={{textAlign:'center'}}>Settings</p>
+        </Menu.Item>
+        <Menu.Item className='center' as={ Link } name="SignOut" to={routes.LOGIN} onClick={signOut} >
+          <p style={{textAlign:'center'}}>Sign Out</p>
+        </Menu.Item>
+      </Menu>
+    </Responsive>
+</div>
 const NavigationNonAuth = () =>
-  <Menu>
-    <Menu.Item as={ Link } name='Home' to={routes.LOGIN}><Header as='h3' color='blue'>Bulb</Header></Menu.Item>
-    <Menu.Item as={ Link } name='Login' to={routes.LOGIN}>Login</Menu.Item>
-    <Menu.Item as={ Link } name='Signup' to={routes.SIGN_UP}>Sign Up</Menu.Item>
-  </Menu>
+  <div>
+    <Responsive as={Menu} {...Responsive.onlyComputer}>
+      <Menu fluid compact>
+        <Menu.Item as={ Link } name='Home' to={routes.LOGIN}><Header as='h3' color='blue'>Bulb</Header></Menu.Item>
+        <Menu.Item as={ Link } name='Login' to={routes.LOGIN}>Login</Menu.Item>
+        <Menu.Item as={ Link } name='Signup' to={routes.SIGN_UP}>Sign Up</Menu.Item>
+      </Menu>
+    </Responsive>
+    <Responsive as={Menu} {...Responsive.onlyMobile}>
+      <Menu fluid vertical>
+        <Menu.Menu>
+          <Menu.Item as={ Link } name='Home' to={routes.LOGIN}>
+            <Header style={{ textAlign:'center' }} as='h2' color='blue'>Bulb</Header>
+          </Menu.Item>
+        </Menu.Menu>
+        <Menu.Item as={ Link } name='Login' to={routes.LOGIN}>
+          <p style={{textAlign:'center'}}>Login</p>
+        </Menu.Item>
+        <Menu.Item as={ Link } name='Signup' to={routes.SIGN_UP}>
+          <p style={{textAlign:'center'}}>Sign Up</p>
+        </Menu.Item>
+      </Menu>
+    </Responsive>
+  </div>
 
 class NavigationHeader extends Component {
   render() {
